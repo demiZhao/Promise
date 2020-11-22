6f7f8b4bfef9b875e46a3e1da2b7fe5515c436c8
demiZhao <demi.y.zhao@gmail.com>
2020-11-22 12:18:11 -0500

    sample

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const output = document.getElementById("app");
+const spinner = document.getElementById("spinner");
+const URL = `https://swapi.dev/api/`;
+const getFilms = (films) => {
+  return films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map((film) => `${film.episode_id} ${film.title}`)
+    .join("\n");
+};
+
+fetch(URL + "films/")
+  // Promise.resolve($.getJSON(URL + "films/"))
+  .then((resp) => {
+    //console.log(resp.ok);
+    if (!resp.ok) {
+      return Promise.reject(new Error("fail to fetch!"));
+    }
+    return resp.json();
+  })
+  .then((data) => data.results)
+  .then((films) => {
+    output.innerText = getFilms(films);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    spinner.remove();
+  });
+
+const delay = (ms) => {
+  return new Promise((resolve, reject) => {
+    throw new Error("boom!");
+    setTimeout(resolve, ms);
+  });
+};
+
+delay(3000)
+  .then(() => console.log("resolved"))
+  .catch((err) => console.log(err));
