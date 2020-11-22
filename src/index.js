ef29ddd21f1adda56931a123f55fc9ef3826f32d
demiZhao <demi.y.zhao@gmail.com>
2020-11-22 16:06:45 -0500

    async and await

--- src/index.js
+++ src/index.js
@@ -30,13 +30,66 @@ fetch(URL + "films/")
     spinner.remove();
   });
 
-const delay = (ms) => {
+const delay = (ms, value) => {
   return new Promise((resolve, reject) => {
-    throw new Error("boom!");
-    setTimeout(resolve, ms);
+    //throw new Error("boom!");
+    setTimeout(() => resolve(value), ms);
   });
 };
 
 delay(3000)
   .then(() => console.log("resolved"))
   .catch((err) => console.log(err));
+
+const timeoutPromise = (ms, promise) => {
+  let timeoutId;
+  const timedOutPromise = new Promise((_, reject) => {
+    timeoutId = setTimeout(() => {
+      reject(new Error("boom!"));
+    }, ms);
+  });
+
+  return Promise.race([promise, timedOutPromise]).finally(() =>
+    clearTimeout(timeoutId)
+  );
+};
+
+const p1 = delay(5000, "A");
+timeoutPromise(1000, p1)
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+const queryData = (url) => {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+};
+
+const queryData = async (url) => {
+  const resp = await fetch(url);
+  if (!resp.ok) {
+    throw Error("boom!");
+  }
+  return resp.json();
+};
+
+// Promise.all([?
+
+//Async & Await
+const main = async () => {
+  try {
+    const [films, people] = await Promise.all([
+      queryData("https://swapi.dev/api/films/"),
+      queryData("https://swapi.dev/api/people/")
+    ]);
+    output.innerText = `${films.count} films ${people.count} people`;
+  } catch (err) {
+    output.innerText = ":(";
+  } finally {
+    spinner.remove();
+  }
+};
+
+main();
